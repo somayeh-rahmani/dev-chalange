efe2d69b409b763e943e841b1faffbfe929cad41
Somayeh Rahmani <rahmani.it27@gmail.com>
2024-12-17 13:44:45 +0330

    write again code with workshop content

--- server.js
+++ server.js
@@ -1,49 +1,43 @@
 const http = require("http");
-const { readFile } = require("fs");
+const { readFile } = require("fs/promises");
 const path = require("path");
 const lookup = require("mime-types").lookup;
 
-// Read files into an object
-const files = {
-  "/": "./index.html",
-  "/product": "./product.html",
-  "/productStyle": "./static/productStyle.css",
-  "/style": "./static/style.css",
-  "/productLogic": "./static/productLogic",
-  "/cactus_img": "./cactus_img.jpeg",
-};
-
-// Function to read a file based on the filename
-function serveFile(filePath, res) {
-  const contentType = lookup(filePath);
-
-  readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write("<h1>File Not Found</h1>");
-      res.end();
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.write(data);
-      res.end();
-    }
-  });
-}
-
 // Create the server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const url = req.url;
-  console.log(url);
 
-  if (files[url]) {
-    serveFile(files[url], res);
-  } else if (url.startsWith("/static/")) {
-    const filePath = path.join(__dirname, url);
-    serveFile(filePath, res);
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.write("<h1>Page Not Found</h1>");
+  if (url === "/") {
+    const content = await readFile("./index.html", "utf8");
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(content);
     res.end();
+    return;
+  }
+
+  if (url === "/index.html" || url === "/index") {
+    const content = await readFile("./index.html", "utf8");
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(content);
+    res.end();
+    return;
+  }
+
+  if (url === "/product" || url === "/product.html") {
+    const content = await readFile("./product.html", "utf8");
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(content);
+    res.end();
+    return;
+  }
+
+  if (url.startsWith("/static")) {
+    const filePath = path.join("./static", url);
+    const content = await readFile(filePath);
+    res.writeHead(200, { "content-type": lookup(filePath) });
+    res.write(content);
+    res.end();
+    return;
   }
 });
 
